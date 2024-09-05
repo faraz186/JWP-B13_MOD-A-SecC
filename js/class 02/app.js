58f1a16161af918737c34d7ad6f8811a13d6232b
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2024-09-05 20:41:37 +0500

    Add files via upload

---
+++ js/class 02/app.js
@@ -0,0 +1,149 @@
+// variables
+
+var name = "faraz";
+
+var num = 1311;
+
+alert(num);
+
+console.log(num);
+
+document.write(num);
+
+// 121321
+
+// data types
+
+// 3214351
+
+// "hello user"
+
+// variable declaration
+
+var age;
+
+console.log(age);   // undefined
+
+
+// variable declaration and initialization
+
+
+var age = 20;
+
+console.log(age);  // output:20
+
+
+
+var firstname = "Muhammad";
+
+var lastname = "faraz";
+
+console.log(firstname + " " + lastname);
+
+
+
+
+var firstname = "Muhammad";
+
+var lastname = "faraz";
+
+var city = "karachi";
+
+console.log("My name is Muhammad Faraz. I lived in karachi.");
+
+console.log(
+  "My name is " + firstname + " " + lastname + ". I lived in " + city
+);
+
+
+var age = "50";
+
+age = 20;
+
+console.log(typeof age);
+
+
+
+var age = 50;
+
+age = 20;
+
+console.log(typeof age);
+
+
+
+var age = 50;
+
+age = 20;
+
+console.log(age);
+
+
+
+var greet = "Hello User's";
+
+console.log(greet);
+
+
+
+var num = 20;
+
+num = 30
+
+console.log(num);
+
+
+
+var num = "10";
+
+var num2 = "20";
+
+console.log(num * num2);
+
+
+
+var num = "10";
+
+var num2 = "20";
+
+console.log(num + num2);
+
+
+
+var a = "faraz";
+
+var b = 20;
+
+console.log(a / b);
+
+
+var a = "faraz";
+
+var b = 20;
+
+console.log(a - b);
+
+
+var a = "faraz";
+
+var b = 20;
+
+console.log(a + b);
+
+
+
+// Camel Case
+
+var firstName = 20;
+
+var lastName = 20;
+
+var fullName = 20;
+
+// Snake Case
+
+var first_name = 20;
+
+var last_name = 20;
+
+var full_name = 20;
