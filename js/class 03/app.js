5ab8581b5b0cc4542b4a365013ba65d43e617ca8
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2024-09-09 21:12:43 +0500

    Add files via upload

---
+++ js/class 03/app.js
@@ -0,0 +1,144 @@
+// camel Case
+
+var fullName;
+
+// snake Case;
+
+var last_name;
+
+// CHpter no 5
+
+var exp = 2 / 4;
+
+console.log(exp);
+
+
+
+var num = 5;
+
+console.log("before value of num: ", num);
+
+var result = ++num;
+
+console.log("After value of num: ", result);
+
+
+
+
+// post-increment   /   post-decrement
+
+// num++              num--
+
+//    +1            /          -1
+
+// pre-increment   /   pre-decrement
+
+// ++num                   --num
+
+// +1              /        -1
+
+
+
+var num = 2;
+
+console.log("before value of num: ", num);
+
+var result = ++num;
+
+
+
+console.log("After value of num: ", result);
+
+
+
+// num++ , num-- // after add or sub
+
+// ++num, --num  // first add or sub
+
+
+
+
+var a = 3;
+
+var b = 5;
+
+var c = 2;
+
+var exp = a-- + --b + c - a + ++c - b-- - ++a - b++
+
+         + ++c + a++ - b + --c;
+
+console.log("exp", exp);
+
+
+
+var exp = 3 * 9 / (10 - 2) - 10 - 2;
+
+
+//  BODMAS
+
+// BRACKET,ORDER,DIVISION,MULTI,ADD,SUB
+
+console.log(exp)
+
+
+
+
+var userInput = prompt(); // js builtin function
+
+console.log("userInput", userInput);
+
+
+
+var firstName = "Muhammad";
+
+var lastName = "faraz";
+
+console.log(firstName, lastName);
+
+
+
+var firstName = prompt("Enter your first name");
+
+var lastName = prompt("Enter your last name");
+
+console.log(firstName + " " + lastName);
+
+
+
+
+// Number()
+
+var num1 = Number(prompt("Enter first number"));
+
+var num2 = Number(prompt("Enter second number"));
+
+console.log(num1 + num2);
+
+
+
+// parseInt()
+
+
+var num1 = parseInt(prompt("Enter first number"));
+
+var num2 = parseInt(prompt("Enter second number"));
+
+console.log(num1 + num2);
+
+
+
+
+// Number(); // JS Builtin constructor function
+
+// parseInt() // JS Builtin function
+
+// +
+
+
+
+alert("Welcome user. \n Happy Coding");
+
+console.log("Welcome user. \n Happy Coding");
+
+document.write("Welcome user. <br> Happy Coding");
