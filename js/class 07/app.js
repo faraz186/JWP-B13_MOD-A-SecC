cfc29d196ceda1078dc313587652b54e4c430eef
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2024-09-23 23:05:29 +0500

    Update app.js

--- js/class 07/app.js
+++ js/class 07/app.js
@@ -1,8 +1,8 @@
 // For Loop
 
-// for (var i = 0;i < 10; i++) {
-//   console.log(i);
-// }
+for (var i = 0;i < 10; i++) {
+  console.log(i);
+}
 
 // for (var i = 9; i >= 0; i--) {
 //   console.log(i);
