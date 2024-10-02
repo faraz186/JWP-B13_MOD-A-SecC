51734246990f5b27f87ba3914360fd9d84a559fe
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2024-10-02 18:27:33 +0500

    Add files via upload

---
+++ js/class 09/app.js
@@ -0,0 +1,78 @@
+Math; // JS Builtin Object
+
+// var number = -12.95;
+
+// console.log(Math.round(number));
+
+// var number = 5.9;
+
+// console.log(Math.ceil(number));
+
+// var number = parseFloat(prompt("Enter any number"));
+
+// console.log("round", Math.round(number));
+// console.log("ceil", Math.ceil(number));
+// console.log("floor", Math.floor(number));
+
+// console.log(Math.round(Math.random() * 132131313));
+
+// console.log(1 < 2 < 3);
+
+// console.log(8 > 7 > 6 >1);
+
+// var num = 24;
+
+// console.log(num.toString());
+
+// var random = Math.random();
+
+// console.log(Number(random.toFixed(4)));
+
+// Palindrome Program
+
+// var input = prompt("Enter a word");
+// var reverseWord = "";
+
+// for (var i = input.length - 1; i >= 0; i--) {
+//   reverseWord = reverseWord + input[i];
+// }
+
+// if (input === reverseWord) {
+//   console.log("it's a palindrome word");
+// } else {
+//   console.log("it's not a palindrome word");
+// }
+
+// var arr = [10, 20, 30, 40, 50, 60, 70];
+
+// for (var i = 0; i < arr.length; i++) {
+//   console.log(arr[i]);
+// }
+
+// var arr = [10, "faraz", 50, false, "ali"];
+
+// console.log(arr.join("       "));
+
+// var str = "hello my name is faraz";
+
+// console.log(str.includes("ab"));
+
+// console.log(str.split(" "));
+
+// var headUser = prompt("Enter head username");
+
+// var tailUser = prompt("Enter tail username");
+
+// var toss = Math.floor(Math.random() * 6 + 1);
+
+// console.log(toss);
+
+// if (toss === 0) {
+//   alert(headUser + "win the toss");
+// } else {
+//   alert(tailUser + "win the toss");
+// }
+
+
+// var randomValues = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%&*"
+
