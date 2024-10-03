3146025070b8e2c3a84c5a3b8f45f15e06129c53
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2024-10-03 21:49:27 +0500

    Add files via upload

---
+++ js/class 10/app.js
@@ -0,0 +1,55 @@
+// var randomValues =
+//   "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%&*";
+
+// var randomPassword = "";
+
+// for (var i = 0; i < 10; i++) {
+//   var randomNumber = Math.round(Math.random() * randomValues.length);
+//   randomPassword += randomValues[randomNumber];
+// }
+
+// console.log(randomPassword);
+
+// Date chap 31
+
+// var date = new Date();
+
+// console.log(date.getTime());
+
+// console.log(typeof date);
+
+// console.log(date.slice(0, 10));
+
+// console.log(date.toString().slice(0, 10));
+
+// var daysArr = ["اتوار", "پیر", "منگل", "بدھ", "جمعرات", "جمعہ", "ہفتہ"];
+
+// console.log(daysArr[date.getDay()]);
+
+// var date = new Date("25 Feb 2015 15:35:40");
+
+// console.log(date.toLocaleTimeString());
+
+// console.log(date);
+
+// var firstRamadan = new Date("March 1, 2025").getTime();
+
+// var current = new Date().getTime();
+
+// var diff = firstRamadan - current;
+
+// var month = diff / (1000 * 60 * 60 * 24 * 30);
+
+// console.log(Math.round(month));
+
+// console.log("firstRamadan", firstRamadan);
+
+// console.log("current", current);
+
+function date() {
+  var date = new Date().getFullYear();
+  console.log(date);
+}
+
+
+date();
